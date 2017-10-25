fb3a6517bc4eb38d60cf370d09e5438f65a77569
Henrik Kniberg <h@kniberg.com>
2017-10-25 23:14:21 +0200

    Async

--- src/tunneltest.js
+++ src/tunneltest.js
@@ -13,53 +13,66 @@ let devicesThatDontWork = []
 
 console.log("\n====================================================================")
 console.log("Connecting to DB...")
+
 connect(config.mongoUrl)
 
-  .then((db) => {
-    console.log("Getting devices...");
-    dbConnection = db
+.then((db) => {
+  console.log("Getting devices...");
+  dbConnection = db
 
-    const Devices = db.collection('devices')
-    return Devices.find({}, {deviceId: 1, sshTunnelPort: 1, updaterVersion: 1})
+  const Devices = db.collection('devices')
+  return Devices.find({}, {deviceId: 1, sshTunnelPort: 1, updaterVersion: 1})
 
-  }).then((cursor) => {
+}).then((cursor) => {
 
-    return cursor.toArray()
+  return cursor.toArray()
 
-  }).then((devices) => {
-    console.log("We have " + devices.length + " devices.")
+}).then((devices) => {
+  console.log("We have " + devices.length + " devices.")
 
-    devicesWithCorrectUpdaterVersion = devices.filter((device) => {
-      return device.updaterVersion == config.expectedUpdaterVersion
-    })
-
-    console.log(devicesWithCorrectUpdaterVersion.length + " of these have updater version " + config.expectedUpdaterVersion)
-    console.log("Connecting to each one...")
-
-    devicesWithCorrectUpdaterVersion.forEach((device) => {
-      if (doesConnectionWork(device.sshTunnelPort)) {
-        process.stdout.write(".")
-        devicesThatWork.push(device)
-      } else {
-        process.stdout.write("X")
-        devicesThatDontWork.push(device)
-      }
-    })
-    process.stdout.write("\n\n")
-    console.log(devicesThatWork.length + " have a working SSH tunnel.")
-    console.log("")
-    if (devicesThatDontWork.length > 0) {
-      console.log(devicesThatDontWork.length + " were not reachable:")
-      devicesThatDontWork.forEach((device) => {
-        console.log("  - " + device.deviceId + " (port " + device.sshTunnelPort + ", updater " + device.updaterVersion + ")")
-      })
-    } else {
-      console.log("That's all of them! Yay!")
-    }
-
-    dbConnection.close()
+  devicesWithCorrectUpdaterVersion = devices.filter((device) => {
+    return device.updaterVersion == config.expectedUpdaterVersion
   })
 
+  console.log(devicesWithCorrectUpdaterVersion.length + " of these have updater version " + config.expectedUpdaterVersion)
+  console.log("Connecting to each one...")
+
+  const checkConnectionPromises = []
+
+  devicesWithCorrectUpdaterVersion.forEach((device) => {
+    checkConnectionPromises.push(createCheckConnectionPromise(device))
+  })
+
+  return Promise.all(checkConnectionPromises)
+
+}).then(() => {
+  process.stdout.write("\n\n")
+  console.log(devicesThatWork.length + " have a working SSH tunnel.")
+  console.log("")
+  if (devicesThatDontWork.length > 0) {
+    console.log(devicesThatDontWork.length + " were not reachable:")
+    devicesThatDontWork.forEach((device) => {
+      console.log("  - " + device.deviceId + " (port " + device.sshTunnelPort + ", updater " + device.updaterVersion + ")")
+    })
+  } else {
+    console.log("That's all of them! Yay!")
+  }
+
+  dbConnection.close()
+})
+
+function createCheckConnectionPromise(device) {
+  return new Promise((resolve, reject) => {
+    if (doesConnectionWork(device.sshTunnelPort)) {
+      process.stdout.write(".")
+      devicesThatWork.push(device)
+    } else {
+      process.stdout.write("X")
+      devicesThatDontWork.push(device)
+    }
+    resolve()
+  })
+}
 
 
 function doesConnectionWork(port) {
